a8c36535587a335e40b7fd7a5bd96d6e92e76dfe
ImArnav19 <morearnav019@gmail.com>
2023-09-16 18:35:52 +0530

    removed some balls

--- src/constants/index.js
+++ src/constants/index.js
@@ -65,18 +65,7 @@ import {
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
+
     {
       name:"Django",
       icon:django,
@@ -93,34 +82,8 @@ import {
       name: "React JS",
       icon: reactjs,
     },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
+
+
     {
       name: "docker",
       icon: docker,
